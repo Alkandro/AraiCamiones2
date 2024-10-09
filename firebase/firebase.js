280bf2f66a5e3b2a6c67ce026a72a233f1ce882c
a <a@MacBook-Air-de-a.local>
2024-10-09 20:56:42 +0900

    problema marge:true resuelto

--- firebase/firebase.js
+++ firebase/firebase.js
@@ -1,3 +1,21 @@
+// import app from "firebase/compat/app";
+// import "firebase/compat/firestore";
+
+// import firebaseConfig from "./config";
+
+// class Firebase {
+//   constructor() {
+//     if (!app.apps.length) {
+//       app.initializeApp(firebaseConfig);
+//       app.firestore().settings({ experimentalForceLongPolling: true });
+//     }
+//     this.db = app.firestore();
+//   }
+// }
+
+// const firebase = new Firebase();
+// export default firebase;
+
 import app from "firebase/compat/app";
 import "firebase/compat/firestore";
 
@@ -7,7 +25,12 @@ class Firebase {
   constructor() {
     if (!app.apps.length) {
       app.initializeApp(firebaseConfig);
-      app.firestore().settings({ experimentalForceLongPolling: true });
+
+      // Solo usa experimentalAutoDetectLongPolling
+      app.firestore().settings({
+        experimentalAutoDetectLongPolling: true,
+        merge: true,
+      });
     }
     this.db = app.firestore();
   }
