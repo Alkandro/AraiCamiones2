4c71cc6bb6b56fe51e54fa59535012e838514148
Alkandro <alkandro@hotmail.com>
2024-10-18 19:12:13 +0900

    todo funcionando perfecto

--- views/DetallePlatillo.js
+++ views/DetallePlatillo.js
@@ -1,12 +1,12 @@
 import React, { useContext, useEffect } from "react";
-import { NativeBaseProvider, Text, View, Image, ScrollView } from "native-base";
+import { NativeBaseProvider, Text, View, Image, ScrollView, Button } from "native-base";
 import { useNavigation } from "@react-navigation/native";
 import PedidosContext from "../context/firebase/pedidos/pedidosContext";
 import { Platform, StyleSheet, ImageBackground } from "react-native";
 
 const DetallePlatillo = () => {
   const { platillo } = useContext(PedidosContext);
-  const { nombre, imagen } = platillo;
+  const { nombre, imagen, descripcion, descripcion2, precio, fecha, fecha2 } = platillo;
   const navigation = useNavigation();
 
   useEffect(() => {
@@ -17,70 +17,63 @@ const DetallePlatillo = () => {
       },
       headerTintColor: "white",
     });
-  }, []);
+  }, [nombre, navigation]);
 
-  // Formatear las fechas
   const formatearFecha = (fecha) => {
-    const nuevaFecha = new Date(fecha); // Asegurarse de que es un objeto Date
+    const nuevaFecha = new Date(fecha); 
     return nuevaFecha.toLocaleDateString('es-ES', {
       day: '2-digit',
       month: '2-digit',
       year: 'numeric',
-    }); // Formato: dd/mm/yyyy
+    });
   };
 
   return (
     <NativeBaseProvider>
       <View flex={1} background="black">
-        {/* <ImageBackground
-          source={require("../assets/fotos/smoke.jpg")}
-          resizeMode="cover"
-          style={styles.imagen4}
-          imageStyle={styles.image}
-        > */}
-          <ScrollView contentContainerStyle={styles.scrollViewContent}>
-            <View paddingTop={5}>
-              <Image
-                style={
-                  Platform.OS === "ios"
-                    ? { marginHorizontal: "5%", width: 350, height: 300 }
-                    : { marginHorizontal: "5%", width: 330, height: 300 }
-                }
-                source={{ uri: imagen }}
-                alt="desde firebase"
-                borderRadius={15}
-                borderColor="#fff"
-                borderWidth={0.9}
-              />
+        <ScrollView contentContainerStyle={styles.scrollViewContent}>
+          <View paddingTop={5}>
+            <Image
+              style={
+                Platform.OS === "ios"
+                  ? { marginHorizontal: "5%", width: 350, height: 300 }
+                  : { marginHorizontal: "5%", width: 330, height: 300 }
+              }
+              source={{ uri: imagen }}
+              alt="desde firebase"
+              borderRadius={15}
+              borderColor="#fff"
+              borderWidth={0.9}
+            />
 
-              {/* Estructura con la etiqueta arriba y el valor abajo */}
-              <View marginTop={10}>
-                <Text style={styles.precio2}>Fecha de carga</Text>
-                <Text style={styles.precio3}>{formatearFecha(platillo.fecha2)}</Text>
-              </View>
-
-              <View marginTop={10}>
-                <Text style={styles.precio2}>Horario de salida</Text>
-                <Text style={styles.precio3}>{platillo.precio}</Text>
-              </View>
-
-              <View marginTop={10}>
-                <Text style={styles.precio2}>Lugar de carga</Text>
-                <Text style={styles.precio3}>{platillo.descripcion}</Text>
-              </View>
-
-              <View marginTop={10}>
-                <Text style={styles.precio2}>Lugar de descarga</Text>
-                <Text style={styles.precio3}>{platillo.descripcion2}</Text>
-              </View>
-
-              <View marginTop={10}>
-                <Text style={styles.precio2}>Fecha de entrega</Text>
-                <Text style={styles.precio3}>{formatearFecha(platillo.fecha)}</Text>
-              </View>
+            <View marginTop={10}>
+              <Text style={styles.precio2}>Fecha de carga</Text>
+              <Text style={styles.precio3}>{formatearFecha(fecha2)}</Text>
             </View>
-          </ScrollView>
-        {/* </ImageBackground> */}
+
+            <View marginTop={10}>
+              <Text style={styles.precio2}>Horario de salida</Text>
+              <Text style={styles.precio3}>{precio}</Text>
+            </View>
+
+            <View marginTop={10}>
+              <Text style={styles.precio2}>Lugar de carga</Text>
+              <Text style={styles.precio3}>{descripcion}</Text>
+            </View>
+
+            <View marginTop={10}>
+              <Text style={styles.precio2}>Lugar de descarga</Text>
+              <Text style={styles.precio3}>{descripcion2}</Text>
+            </View>
+
+            <View marginTop={10}>
+              <Text style={styles.precio2}>Fecha de entrega</Text>
+              <Text style={styles.precio3}>{formatearFecha(fecha)}</Text>
+            </View>
+
+            
+          </View>
+        </ScrollView>
       </View>
     </NativeBaseProvider>
   );
@@ -88,7 +81,7 @@ const DetallePlatillo = () => {
 
 const styles = StyleSheet.create({
   scrollViewContent: {
-    paddingBottom: 230, // Esto da espacio extra al final para que todo el contenido sea visible al hacer scroll
+    paddingBottom: 230,
     alignItems: 'center',
   },
   precio2: {
@@ -97,7 +90,7 @@ const styles = StyleSheet.create({
     fontSize: 20,
     fontWeight: "bold",
     color: "white",
-    textDecorationLine:"underline"
+    textDecorationLine: "underline",
   },
   precio3: {
     marginVertical: 2,
@@ -105,14 +98,15 @@ const styles = StyleSheet.create({
     fontSize: 20,
     fontWeight: "bold",
     color: "#FFDA00",
-    marginTop:10,
+    marginTop: 10,
   },
-  imagen4: {
-    flex: 1,
-    justifyContent: "center",
+  boton2: {
+    backgroundColor: "#FFDA00",
   },
-  image: {
-    opacity: 1,
+  botonTexto2: {
+    color: "black",
+    fontSize: 18,
+    fontWeight: "bold",
   },
 });
 
