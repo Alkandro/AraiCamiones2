b3b257e91dbb7faa7a608bbfe05a85500af7b01e
Alkandro <alkandro@hotmail.com>
2024-11-29 22:52:44 +0900

    drawer funcionando ok2

---
+++ views/users.js
@@ -0,0 +1,17 @@
+// // users.js
+// const users = {
+//     "tomaoka@arai.co.jp": { password: "123456", drawer: "TomaokaDrawer" },
+//     "matsushima@arai.co.jp": { password: "123456", drawer: "MatsushimaDrawer" },
+//     "hoshino@arai.co.jp": { password: "123456", drawer: "HoshinoDrawer" },
+//     "oishi@arai.co.jp": { password: "123456", drawer: "OishiDrawer" },
+//     "okamoto@arai.co.jp": { password: "123456", drawer: "OkamotoDrawer" },
+//     "yamakado@arai.co.jp": { password: "123456", drawer: "YamakadoDrawer" },
+//     "sklar@arai.co.jp": { password: "123456", drawer: "SklarDrawer" },
+//     "enrique@arai.co.jp": { password: "123456", drawer: "EnriqueDrawer" },
+//     "user1@arai.co.jp": { password: "123456", drawer: "User1Drawer" },
+//     "user2@arai.co.jp": { password: "123456", drawer: "User2Drawer" },
+//     "user3@arai.co.jp": { password: "123456", drawer: "User3Drawer" },
+//   };
+  
+//   export default users;
+  
